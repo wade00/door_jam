f959a94351904ac9376755b2d44c44be18cb34a5
Wade Novak <wade@launchpadlab.com>
2016-07-18 13:39:26 -0500

    Adds slack response logic

--- app.rb
+++ app.rb
@@ -4,17 +4,37 @@ require "sinatra"
 require "sinatra/activerecord"
 require "httparty"
 require "json"
+require "dotenv"
+require "base64"
 require "./models/door"
 
 Bundler.require
+Dotenv.load
+
+config = {
+  outgoing_token: ENV["OUTGOING_TOKEN"],
+  slack_team_id:  ENV["SLACK_TEAM_ID"]
+}
 
 get "/check-door" do
-  puts "Checking status..."
-  puts Door.first.status
+  if params[:token] == config[:outgoing_token]
+    door_status = Door.first.status
+    color =  door_status == 'open' ? '37A651' : 'BF4040'
+    body  = { response_type: "ephemeral",
+              text: "Checking the bathroom door...",
+              attachments: [
+                {
+                  color: color,
+                  text: "It's #{Door.first.status}!"
+                }
+              ]
+            }
+
+    response = HTTParty.post(params[:response_url], body: body.to_json)
+  end
 end
 
 get "/update-door/:status" do |status|
-  puts "Hitting update..."
   door = Door.first
   if door
     door.update_attributes({ status: status })
